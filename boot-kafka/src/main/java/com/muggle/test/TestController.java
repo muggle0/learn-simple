2ecc0e7e592480a2a59d3dd90ccec5b8d132330d
muggle <isocket@outlook.com>
2020-04-22 08:50:12 +0800

    kafka 测试

---
+++ boot-kafka/src/main/java/com/muggle/test/TestController.java
@@ -0,0 +1,32 @@
+package com.muggle.test;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/4/2$
+ **/
+@Component
+public class TestController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled(cron = "*/15 * * * * ?")
+    public void send() {
+        kafkaTemplate.send("zhisheng", "test");
+    }
+
+    @KafkaListener(topics = "zhisheng")
+    public void listen(ConsumerRecord<?, ?> record) {
+        Object value = record.value();
+        System.out.println(value);
+    }
+
+}
---
+++ boot-kafka/src/test/java/com/muggle/BootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
