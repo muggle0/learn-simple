3062c4ddaff3155778b51520386ec9593011a222
muggle <isocket@outlook.com>
2020-04-26 18:02:15 +0800

    接口代理测试

---
+++ aop-test/src/main/java/com/muggle/learn/AopTestApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopTestApplication.class, args);
+    }
+
+}
--- webflux-test/src/main/java/com/muggle/webfluxtest/TestController.java
+++ webflux-test/src/main/java/com/muggle/webfluxtest/TestController.java
@@ -1,10 +1,14 @@
 package com.muggle.webfluxtest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.reactive.context.ReactiveWebServerApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 /**
  * @Description:
@@ -14,10 +18,16 @@ import reactor.core.publisher.Mono;
 @RestController
 public class TestController {
     @Autowired
-    WebApplicationContext context;
+    ReactiveWebServerApplicationContext context;
 
     @GetMapping("/")
     public Mono<String> test(){
         return Mono.just("..........");
+    }
+
+    @GetMapping("/socket")
+    public Flux<String> longSocket(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(result -> ">>>");
     }
 }
