c405935091dd486f4cba8ffa243f517dbec6adc0
muggle <isocket@outlook.com>
2020-04-27 20:58:04 +0800

    声明式查询

---
+++ mybatis-plugin-test/src/main/java/com/muggle/learn/config/MyBatisConfig.java
@@ -0,0 +1,17 @@
+package com.muggle.learn.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/4/27
+ **/
+
+@Component
+public class MyBatisConfig {
+    @Value("")
+    private String
+
+}
