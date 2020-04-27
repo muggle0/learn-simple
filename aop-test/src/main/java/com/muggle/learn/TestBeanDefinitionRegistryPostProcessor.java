a8e7398c68ba5be4791072302606c03646bb8eb1
muggle <isocket@outlook.com>
2020-04-27 20:35:18 +0800

    声明式查询

---
+++ aop-test/src/main/java/com/muggle/learn/TestBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,15 @@
+package com.muggle.learn;
+
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/4/26
+ **/
+public class TestBeanDefinitionRegistryPostProcessor implements ImportBeanDefinitionRegistrar {
+    /*
+    * https://www.cnblogs.com/zzq-include/p/8004506.htmlhttps://www.cnblogs.com/zzq-include/p/8004506.html
+    * https://zhuanlan.zhihu.com/p/91461558
+    * */
+}
---
+++ mybatis-plugin-test/src/main/java/com/muggle/learn/mybatisplugintest/MybatisPluginTestApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.learn.mybatisplugintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPluginTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPluginTestApplication.class, args);
+    }
+
+}
