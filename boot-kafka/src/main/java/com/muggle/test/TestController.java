e5268881bfe8054ae6a1276ac5e69f1b58ba7f78
muggle <isocket@outlook.com>
2020-09-25 17:38:46 +0800

    略有心得

--- boot-kafka/src/main/java/com/muggle/test/TestController.java
+++ boot-kafka/src/main/java/com/muggle/test/TestController.java
@@ -1,12 +1,18 @@
 package com.muggle.test;
 
-import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,25 +29,38 @@ public class TestController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
     @Autowired
-    AdminClient client;
+    KafkaProperties kafkaProperties;
+    private   AdminClient adminClient;
+    @PostConstruct
+    public void init(){
+         adminClient = KafkaAdminClient.create(kafkaProperties.buildAdminProperties());
+    }
 
-//    @Scheduled(cron = "*/15 * * * * ?")
+
+    @Scheduled(cron = "*/15 * * * * ?")
     public void send() {
+
         kafkaTemplate.send("xxxxx", "test");
     }
 
-//    @KafkaListener(topics = "xxxxx",groupId = "test-consumer-group")
-    public void listen(ConsumerRecord<?, String> record) {
+    @KafkaListener(topics = "xxxxx",groupId = "test-consumer-group")
+    public void listen(ConsumerRecord<?, String> record) throws ExecutionException, InterruptedException {
         String value = record.value();
+        ListTopicsResult listTopicsResult = adminClient.listTopics();
+        Collection<TopicListing> topicListings = listTopicsResult.listings().get();
         System.out.println(value);
+
     }
 
-    @Scheduled(cron = "*/15 * * * * ?")
-    public void getTopic(){
-        Collection<NewTopic> newTopics = new ArrayList<>(1);
-        newTopics.add(new NewTopic("topic-kl",1,(short) 1));
-        client.createTopics(newTopics);
-    }
+//    @Scheduled(cron = "*/15 * * * * ?")
+//    public void getTopic(){
+//        Collection<NewTopic> newTopics = new ArrayList<>(1);
+//        newTopics.add(new NewTopic("topic-kl",1,(short) 1));
+//        client.createTopics(newTopics);
+//        System.out.println("》》》》》》》》》》》》》》》 创建topic");
+//        ListTopicsResult listTopicsResult = client.listTopics();
+//        System.out.println(">>>>>>>>>>>>>>>>>>>获取列表");
+//    }
 
 //    https://blog.csdn.net/zzpdljd1991/article/details/90794156
 
