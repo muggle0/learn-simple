6e8c599a430de0639e4c7bf13036608d55198034
muggle0 <isocket@outlook.com>
2020-11-25 17:30:31 +0800

    sssssssssss

---
+++ boot-elk/src/main/java/com/muggle/bootelk/BootElkApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.bootelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootElkApplication.class, args);
+    }
+
+}
--- doc/kafka.md
+++ doc/kafka.md
@@ -1,3 +1,4 @@
 # kafka 介绍
+
 # kafka 架构
 # kafka实战
