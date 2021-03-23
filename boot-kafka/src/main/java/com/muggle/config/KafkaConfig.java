91c49514953008447be97ce72838e51d81030059
muggle <isocket@outlook.com>
2021-03-23 23:19:31 +0800

    sendto 配置

--- boot-kafka/src/main/java/com/muggle/config/KafkaConfig.java
+++ boot-kafka/src/main/java/com/muggle/config/KafkaConfig.java
@@ -4,10 +4,20 @@ import javax.annotation.PostConstruct;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description:
@@ -28,4 +38,53 @@ public class KafkaConfig {
     public AdminClient init( KafkaProperties kafkaProperties){
         return KafkaAdminClient.create(kafkaProperties.buildAdminProperties());
     }
+
+    /**
+     * 同步的kafka需要ReplyingKafkaTemplate,指定repliesContainer
+     * @param producerFactory
+     * @param repliesContainer
+     * @return
+     */
+    @Bean
+    public ReplyingKafkaTemplate<String, String, String> replyingTemplate(
+            ProducerFactory<String, String> producerFactory,
+            ConcurrentMessageListenerContainer<String, String> repliesContainer) {
+
+        ReplyingKafkaTemplate template = new ReplyingKafkaTemplate<>(producerFactory, repliesContainer);
+        //同步相应超时时间：10s
+        template.setReplyTimeout(10000);
+        return template;
+    }
+
+    @Bean
+    public ProducerFactory<String,String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"10.128.100.100:9092");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        //发送一次message最大大小，默认是1M,这里设置为20M
+        //props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 20971520);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    /**
+     * 指定consumer返回数据到指定的topic
+     * @return
+     */
+    @Bean
+    public ConcurrentMessageListenerContainer<String, String> repliesContainer(ConcurrentKafkaListenerContainerFactory<String, String> containerFactory) {
+        ConcurrentMessageListenerContainer<String, String> repliesContainer =
+                containerFactory.createContainer("REPLY_ASYN_MESSAGE");
+        repliesContainer.getContainerProperties().setGroupId("replies_message_group");
+        repliesContainer.setAutoStartup(false);
+        return repliesContainer;
+    }
+
 }
