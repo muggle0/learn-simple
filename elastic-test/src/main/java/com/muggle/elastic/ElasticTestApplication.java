b58f744136a6fd70a9b767970f339cbb92351954
muggle <isocket@outlook.com>
2021-09-03 10:28:59 +0800

    feat(1): ws elasticserarch

--- elastic-test/src/main/java/com/muggle/elastic/ElasticTestApplication.java
+++ elastic-test/src/main/java/com/muggle/elastic/ElasticTestApplication.java
@@ -11,13 +11,26 @@ public class ElasticTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticTestApplication.class, args);
-        final RestHighLevelClient restHighLevelClient = new RestHighLevelClient();
-        restHighLevelClient.updateByQuery();
-        restHighLevelClient.update()
-        new UpdateByQueryRequest();
-        final Script script = new Script();
-        script.toString(
-            restHighLevelClient.updateByQuery()
+        /**
+         * final String orgId = userRoleDo.getOrgId();
+         *             final String userId = userRoleDo.getUserId();
+         *             final String roleId = userRoleDo.getRoleId();
+         *             final Long authorityCode = authorityGroup.getAuthorityGroupCode();
+         *             final UpdateByQueryRequestBuilder requestBuilder = UpdateByQueryAction.INSTANCE.newRequestBuilder(esClient);
+         *             Map<String, Object> params=new HashMap<>();
+         *             params.put("authorityCode",authorityCode.toString());
+         *             params.put("billTypeCode",authorityGroupMap.get(authorityCode).getBillTypeCode());
+         *             final StringBuilder scriptCode = new StringBuilder();
+         *             // 更新脚本
+         *             scriptCode.append( "List dataAuths = ((List) ctx._source.dataAuth); for (dataAuthItem in dataAuths) " )
+         *                 .append("{if (dataAuthItem.billTypeCode=params.billTypeCode){List authGrops = ((List) dataAuthItem.authorityGroup); ")
+         *                 .append("authGrops.removeIf(item -> { item.authorityGroupCode == params.authorityCode; })}}");
+         *             final Script script = new Script(ScriptType.INLINE, "painless", scriptCode.toString(), params);
+         *             // 更新条件
+         *             final BoolQueryBuilder must = QueryBuilders.boolQuery().must(QueryBuilders.termQuery("roleId", roleId))
+         *                 .must(QueryBuilders.termQuery("userId", userId)).must(QueryBuilders.termQuery("orgId", orgId));
+         *             requestBuilder.source("auth").script(script).filter(must).abortOnVersionConflict(false).get();
+         */
     }
 
 }
