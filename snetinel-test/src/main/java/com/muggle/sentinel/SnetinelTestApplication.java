5b02e92155bbc477e6197aba2279eb413cee172d
muggle <isocket@outlook.com>
2021-11-08 09:53:41 +0800

    sentinel+sms

--- snetinel-test/src/main/java/com/muggle/sentinel/SnetinelTestApplication.java
+++ snetinel-test/src/main/java/com/muggle/sentinel/SnetinelTestApplication.java
@@ -1,7 +1,7 @@
 package com.muggle.sentinel;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SnetinelTestApplication {
