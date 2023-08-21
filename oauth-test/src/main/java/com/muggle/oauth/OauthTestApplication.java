869a9c62894707e17a3e8d0f91b376eaa5d6ea10
muggle <isocket@outlook.com>
2023-08-21 16:25:28 +0800

    oauth2 测试

---
+++ oauth-test/src/main/java/com/muggle/oauth/OauthTestApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthTestApplication.class, args);
+	}
+
+}
