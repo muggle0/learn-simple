de3ec88e7192820851b327f6dbcade64173e119d
muggle <isocket@outlook.com>
2023-08-21 17:54:21 +0800

    oauth2 测试

---
+++ oauth-test/src/main/java/com/muggle/oauth/auth/AuthorizationServerConfig.java
@@ -0,0 +1,9 @@
+package com.muggle.oauth.auth;
+
+/**
+ * Description
+ * Date 2023/8/21
+ * Created by muggle
+ */
+public class AuthorizationServerConfig {
+}
---
+++ oauth-test/src/main/java/com/muggle/oauth/client/OAuth2LoginConfig.java
@@ -0,0 +1,37 @@
+package com.muggle.oauth.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+/**
+ * Description
+ * Date 2023/8/21
+ * Created by muggle
+ */
+@Configuration
+public class OAuth2LoginConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("google-client-id")
+                .clientSecret("google-client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email", "address", "phone")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .clientName("Google")
+                .build();
+    }
+}
