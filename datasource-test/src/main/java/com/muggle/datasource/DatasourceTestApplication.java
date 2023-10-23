d667c9e9cf5149d1f24b9dff262d32ab8a53cf41
yuanzening <isocket@outlook.com>
2023-10-23 11:45:04 +0800

    oauth2 测试

---
+++ datasource-test/src/main/java/com/muggle/datasource/DatasourceTestApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceTestApplication.class, args);
+    }
+
+}
