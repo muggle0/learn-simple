d4ca54f62c5a07e01110f9abb1bd9f5b333c9905
yuanZeNing <yuanzn189@chinatelecom.cn>
2024-08-07 15:10:21 +0800

    jprotobuf-rpc-test

--- jprotobuf-socket-test/src/main/java/com/muggle/jprotobuf/model/JprotobufRequst.java
+++ jprotobuf-socket-test/src/main/java/com/muggle/jprotobuf/model/JprotobufRequst.java
@@ -1,9 +1,16 @@
 package com.muggle.jprotobuf.model;
 
+import com.baidu.bjf.remoting.protobuf.annotation.Protobuf;
+import lombok.Data;
+
 /**
  * Description
  * Date 2024/8/7
  * Created by muggle
  */
+@Data
 public class JprotobufRequst {
+
+    @Protobuf
+    public String message;
 }
--- jprotobuf-socket-test/src/main/java/com/muggle/jprotobuf/server/JprotobufServer.java
+++ jprotobuf-socket-test/src/main/java/com/muggle/jprotobuf/server/JprotobufServer.java
@@ -1,9 +1,19 @@
 package com.muggle.jprotobuf.server;
 
+import com.baidu.jprotobuf.pbrpc.ProtobufRPCService;
+import com.muggle.jprotobuf.clinet.JprotobufClient;
+import com.muggle.jprotobuf.model.JprotobufRequst;
+import com.muggle.jprotobuf.model.JprotobufResponse;
+
 /**
  * Description
  * Date 2024/8/7
  * Created by muggle
  */
-public class JprotobufServer {
+public class JprotobufServer implements JprotobufClient {
+    @ProtobufRPCService(serviceName = "echoService", methodName = "echo")
+    @Override
+    public JprotobufResponse echo(JprotobufRequst requst) {
+        return null;
+    }
 }
