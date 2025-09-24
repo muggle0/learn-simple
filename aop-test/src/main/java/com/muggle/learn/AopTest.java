217399d8cc84a040833dc3b54ab52c2a740841e9
muggle <isocket@outlook.com>
2025-09-24 11:34:31 +0800

    开始写状态机

--- aop-test/src/main/java/com/muggle/learn/AopTest.java
+++ aop-test/src/main/java/com/muggle/learn/AopTest.java
@@ -1,3 +1,5 @@
+package com.muggle.learn;
+
 //package com.muggle.learn;
 //
 //import com.muggle.learn.annotation.TestQuery;
@@ -36,3 +38,69 @@
 //    }
 //
 //}
+public class AopTest {
+
+    public static void main(String[] args) {
+        final ListNode listNode1 = new ListNode(1);
+        final ListNode listNode2 = new ListNode(2);
+        final ListNode listNode3 = new ListNode(3);
+        final ListNode listNode4 = new ListNode(4);
+        listNode1.next=listNode2;
+        listNode2.next=listNode3;
+        listNode3.next=listNode4;
+        final ListNode flip = flip(listNode1);
+        System.out.println(flip.val);
+        System.out.println(flip.next.next.val);
+        System.out.println(19%10);
+        int val1=0;
+        ListNode listNode=extracted(flip, val1,null);
+
+    }
+
+    private static ListNode extracted(ListNode flip, int val1,ListNode l) {
+         ListNode listNode = new ListNode((flip.val + 1+val1) % 10);
+        if (l==null){
+            l=listNode;
+        }else {
+            l.next=listNode;
+        }
+            val1=(flip.val+1)/10;
+        if (flip.next!=null){
+            extracted(flip.next,val1,listNode);
+        }
+        return l;
+    }
+
+    public static ListNode flip(ListNode li){
+        ListNode temp=li.next;
+        ListNode result=new ListNode(0);
+//        li.next=null;
+        while (result!=null){
+            result=subFilp(li,temp);
+            if (result!=null){
+                li=temp;
+                temp=result;
+            }
+        }
+        return  temp;
+    }
+    static ListNode subFilp(ListNode li, ListNode temp) {
+        final ListNode next = temp.next;
+        temp.next=li;
+        return next;
+    }
+}
+
+class ListNode{
+    int val;
+
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+    }
+}
+class Solution{
+
+
+
+}
